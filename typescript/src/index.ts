ac8e11c8bc7a4c4909e72d18b1f1366a2dc9759f
HarleQu16 <130879118+HarleQu16@users.noreply.github.com>
2024-09-06 11:29:48 -0400

    add function computeArea to Shape interface and get rid of exporting variables in corresponding shapes, deleted rectangle interface, change rectangle to shape in renderer

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,7 @@
 import { newRenderer } from "./renderer.js"
 import { newRectangle } from "./shapes/rectangle.js";
 
+//
 const rectangle: Shape = newRectangle(2, 3)
 const renderer = newRenderer(rectangle)
 renderer.draw();
