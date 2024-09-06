5d9395db4090edd133988612be105fe6f0611631
HarleQu16 <130879118+HarleQu16@users.noreply.github.com>
2024-09-06 11:27:03 -0400

    add function getArea to Shape interface and change public variables into private in all three shapes correspondingly. Change Rectangle to Shape in Renderer

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -2,6 +2,7 @@ package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 
+//
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
